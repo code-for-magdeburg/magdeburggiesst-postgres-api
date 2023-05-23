edd1241ef2b3626d04f8b782cb25e82dec51577e
Jens Winter-Hübenthal <jens.winter@gmail.com>
2023-05-23 19:34:05 +0200

    Squashed commit of the following:
    commit a56488036e79f5667994929d7d6e95604a8b51bd
    Author: Jens Winter-Hübenthal <jens.winter@gmail.com>
    Date:   Tue May 23 19:18:10 2023 +0200
    
        Modify package.json
    
    commit 65089ba9ed8a4dc7cb41baa5e2a74f5094fca89f
    Merge: 480f9db d7bea78
    Author: Jens Winter-Hübenthal <jens.winter@gmail.com>
    Date:   Tue May 23 18:51:22 2023 +0200
    
        Merge branch 'master' into port-to-md
    
    commit 480f9db55893412fe8c6367e156ddea0e229554c
    Author: Jens Winter-Hübenthal <jens.winter@gmail.com>
    Date:   Sat May 20 21:40:46 2023 +0200
    
        adds trigger branch "port-to-md"
    
    commit 85a6edcd74b08c832cf5c585206220d765d01760
    Merge: a2d96d9 f2d9add
    Author: Jens Winter-Hübenthal <jens.winter@gmail.com>
    Date:   Fri May 12 19:01:09 2023 +0200
    
        Merge branch 'staging' into port-to-md
    
    commit a2d96d9387e5854f85407f3e034ff4a3e695c8f4
    Merge: b249cb6 a8a97e4
    Author: Jens Winter-Hübenthal <jens.winter@gmail.com>
    Date:   Thu May 11 20:55:38 2023 +0200
    
        Merge branch 'staging' into port-to-md
    
    commit b249cb6eb08492462ad8dff8beb3779237bd1a06
    Author: Jens Winter-Hübenthal <jens.winter@gmail.com>
    Date:   Wed May 10 19:04:24 2023 +0200
    
        production environment
    
    commit 54df08528d0736a5b6564d3d76ac5923658ae68d
    Author: Jens Winter-Hübenthal <jens.winter@gmail.com>
    Date:   Wed May 10 17:46:30 2023 +0200
    
        deployment github workflows
    
    commit ce00a353276fe32f339c1677056dd5bd7f922cdf
    Author: Jens Winter-Hübenthal <jens.winter@gmail.com>
    Date:   Tue May 9 20:39:43 2023 +0200
    
        adds radolan geometry for Magdeburg
    
    commit e29fae24588c9c2d35e0ee6ffcc32d6d385bfa65
    Author: Jens Winter-Hübenthal <jens.winter@gmail.com>
    Date:   Tue May 9 20:37:31 2023 +0200
    
        Reset seed.sql
    
    commit e02fd373fc99b1d0a18a5bc5abca6c8dc90a24cd
    Author: Jens Winter-Hübenthal <jens.winter@gmail.com>
    Date:   Fri May 5 20:03:01 2023 +0200
    
        Fixes title
    
    commit efbca54c8023be59f6ef67e01e1b5152af6bb3ea
    Author: Jens Winter-Hübenthal <jens.winter@gmail.com>
    Date:   Fri May 5 19:58:34 2023 +0200
    
        adds deployment workflow
    
    commit e078ffe0f1551d3f578a4401939a70010759db2e
    Author: Jens Winter-Hübenthal <jens.winter@gmail.com>
    Date:   Fri May 5 19:42:26 2023 +0200
    
        removes pages folder
    
    commit 194c9dc8b583e9a8ed9a40db20b2245224f824a0
    Merge: dd2b303 339f243
    Author: Jens Winter-Hübenthal <jens.winter@gmail.com>
    Date:   Mon Apr 24 18:15:43 2023 +0200
    
        Merge branch 'staging' into port-to-md
    
    commit dd2b30349c5dc8d973d3b421e7540ea9bc82a876
    Merge: 7355a8c c2c0a2c
    Author: Jens Winter-Hübenthal <jens.winter@gmail.com>
    Date:   Thu Apr 20 21:08:13 2023 +0200
    
        Merge branch 'staging' into port-to-md
    
    commit 7355a8cfb0aec7e8014c2766cb920c7e168a2859
    Author: Jens Winter <jens.winter@gmail.com>
    Date:   Wed Mar 29 21:41:31 2023 +0200
    
        radolan geometry data for Magdeburg
    
    commit e5bc5a448d41e5d98cd1b9f5f822d75d6255bd19
    Merge: 6057e60 dbffdf6
    Author: Jens Winter <jens.winter@gmail.com>
    Date:   Wed Mar 29 21:39:55 2023 +0200
    
        Merge branch 'staging' into port-to-md
    
    commit 6057e60f310227a532a03109532ba1636f58532a
    Author: Jens Winter <jens.winter@gmail.com>
    Date:   Sun Mar 26 17:29:23 2023 +0200
    
        add missing pages folder

--- release.config.cjs
+++ release.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
 	extends: "@technologiestiftung/semantic-release-config",
 	branches: [
 		{ name: "master" },
+		{ name: "master-magdeburg" },
 		{ name: "staging", channel: "pre/rc", prerelease: "rc" },
 	],
 	plugins: [
